5beb619c5ce34bc8934c43288239af0c3a4fad22
chenguangjie111 <979230865@qq.com>
2019-07-23 21:14:00 +0800

    每个身份每期开放的人数
    每个身份每期开放的人数

--- application/mobile/controller/Cart.php
+++ application/mobile/controller/Cart.php
@@ -323,27 +323,34 @@ class Cart extends MobileBase {
             $time=time();
             Db::name('users')->update(['user_id'=>$order['user_id'],'agent_level'=>$order['agent_good'],'default_period'=>1,'add_agent_time'=>$time]);
 
+            $pop_num_area= $confModel->where('name','=','pop_num_area')->value('value');
+            $pop_num_city= $confModel->where('name','=','pop_num_city')->value('value');
+            $pop_num_province= $confModel->where('name','=','pop_num_province')->value('value');
              if($order['agent_good']==1){
                  $pop_name='pop_person_num';
+                 $pop_num=$pop_num_area;
              }
              if($order['agent_good']==2){
                 $pop_name='pop_person_num_city';
+                $pop_num=$pop_num_city;
              }
              if($order['agent_good']==3){
                 $pop_name='pop_person_num_province';
+                $pop_num=$pop_num_province;
              }
+
             $pop_person_num=Db::name('config')->where('name','=',$pop_name)->value('value');
-            $period_count=ceil($pop_person_num/12);
+            $period_count=ceil($pop_person_num/$pop_num);
             static $current_num='';
             $current_num=$pop_person_num;
             $popPeriodModel=Db::name('pop_period');
            for($i=1;$i<=$period_count;$i++){
-                if($current_num>12){
-                    $current_num-=12;
+                if($current_num>$pop_num){
+                    $current_num-=$pop_num;
                     if($i==1){
-                        $popPeriodModel->insert(['user_id'=>$order['user_id'],'person_num'=>12,'poped_per_num'=>0,'period'=>$i,'level'=>$order['agent_good'],'begin_time'=>$time,'end_time'=>'']);
+                        $popPeriodModel->insert(['user_id'=>$order['user_id'],'person_num'=>$pop_num,'poped_per_num'=>0,'period'=>$i,'level'=>$order['agent_good'],'begin_time'=>$time,'end_time'=>'']);
                     }else{
-                        $popPeriodModel->insert(['user_id'=>$order['user_id'],'person_num'=>12,'poped_per_num'=>0,'period'=>$i,'level'=>$order['agent_good'],'begin_time'=>'','end_time'=>'']);
+                        $popPeriodModel->insert(['user_id'=>$order['user_id'],'person_num'=>$pop_num,'poped_per_num'=>0,'period'=>$i,'level'=>$order['agent_good'],'begin_time'=>'','end_time'=>'']);
                     }
                 }else{
                     $popPeriodModel->insert(['user_id'=>$order['user_id'],'person_num'=>$current_num,'poped_per_num'=>0,'period'=>$i,'level'=>$order['agent_good'],'begin_time'=>'','end_time'=>'']);
