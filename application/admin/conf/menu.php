485d2c3fb477530ef556b9d9f3403786a7429cdf
sonelyx <snowxtlyx@163.com>
2019-07-23 09:59:53 +0800

    xiugai

--- application/admin/conf/menu.php
+++ application/admin/conf/menu.php
@@ -182,7 +182,7 @@ return	array(
 			// array('name'=>'会员等级','act'=>'levelList','op'=>'User'),
 		)),
 		array('name' => '充值提现','child'=>array(
-			array('name'=>'充值记录','act'=>'recharge','op'=>'User'),
+			// array('name'=>'充值记录','act'=>'recharge','op'=>'User'),
 			array('name'=>'提现申请','act'=>'withdrawals','op'=>'User'),
 			array('name'=>'提现设置','act'=>'cash','op'=>'System'),
 		)),
